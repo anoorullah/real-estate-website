7e1d55036a5cce25b43c02f775e1435c176494c4
Noorullah <adnann2@illinois.edu>
2022-05-27 01:31:03 -0500

    Fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
 import "./app.scss"
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
