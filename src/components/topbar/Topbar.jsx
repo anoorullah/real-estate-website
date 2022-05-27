1d1d587b54f8d75e5179f79a7f26f1e899140a83
Noorullah <adnann2@illinois.edu>
2022-05-27 00:07:47 -0500

    Started Menu design and functionality

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,27 @@
 import "./topbar.scss";
 import {CameraFront, Email, Campaign, AssuredWorkload, House} from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar( { menuOpen, setMenuOpen } ) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo"> genius. </a>
           <div className="itemContainer">
             <CameraFront className="icon" />
-            <span>+44 924 12 74</span>
+            <span>+1 847-975-4732</span>
           </div>
           <div className="itemContainer">
             <Email className="icon" />
-            <span>noorullah@hotmail.com</span>
+            <span>adnan.noorullah@gmail.com</span>
           </div>
         </div>
         <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
